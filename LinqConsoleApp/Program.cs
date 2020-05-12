768e19fbbfea84462866158f685b42b6cd6cfe73
Zaborejszyn <zabor1274@gmail.com>
2020-05-12 23:16:42 +0200

    Initial commit

---
+++ LinqConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace LinqConsoleApp {
+    class Program {
+        static void Main(string[] args) {
+            var d = new LinqSamples();
+            d.Przyklad7();
+        }
+    }
+}
